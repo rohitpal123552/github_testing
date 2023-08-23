3f7e9056e530f136ee0d2e7a27f1afd1169859e6
rohitpal123552 <51694429+rohitpal123552@users.noreply.github.com>
2023-08-23 22:10:57 +0530

    Create mymath.go

---
+++ mymath/mymath.go
@@ -0,0 +1,11 @@
+package mymath
+
+// Add adds two integers and returns the result.
+func Add(a, b int) int {
+	return a + b
+}
+
+// Subtract subtracts two integers and returns the result.
+func Subtract(a, b int) int {
+	return a - b
+}
